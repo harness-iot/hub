966dc0309ef19bb524130389d4525a6767424135
tilersmyth <tyler.smith.la@gmail.com>
2022-05-06 16:20:46 +0100

    Change graphql endpoint

--- packages/api/src/graphql/graphql.service.ts
+++ packages/api/src/graphql/graphql.service.ts
@@ -16,7 +16,7 @@ export class GraphqlService implements GqlOptionsFactory {
 
   createGqlOptions(): ApolloDriverConfig {
     return {
-      path: 'harriot',
+      path: 'core',
       // introspection: true,
       // cors: {
       //   origin: true,
