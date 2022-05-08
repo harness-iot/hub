09c278057973f7086be2407d2da6259ccb2a3547
tilersmyth <tyler.smith.la@gmail.com>
2022-05-08 22:22:47 +0100

    testing version deploy

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -14,11 +16,27 @@ async function bootstrap() {
     const configService = app.get(ConfigService);
     await app.listen(configService.PORT, configService.BASE_HOST);
 
-    // testing ci
-
     // Start BLE advertising
     const ble = app.get(BleService);
     ble.start();
+
+    // Check if first start since install or update
+    const configRaw = fs.readFileSync('../config.json', 'utf8');
+    const config = JSON.parse(configRaw);
+
+    if (
+      config.state === 'initial_boot' ||
+      config.state === 'update_successful'
+    ) {
+      const lernaRaw = fs.readFileSync(
+        `${this.configService.BASE_DIR}/lerna.json`,
+        'utf8',
+      );
+      const lerna = JSON.parse(lernaRaw);
+      config['version'] = lerna.version;
+      config['state'] = 'running';
+      fs.writeFileSync('../config.json', JSON.stringify(config, null, 2));
+    }
   } catch (err) {
     const logger = new LoggerService();
     logger.error('Intrakit Launch Failed!', err);
